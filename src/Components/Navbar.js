4fa20702d904a34ec152493872dcb1b19085a362
Prakhar Gupta <prakgup20@gmail.com>
2023-06-14 18:08:59 +0530

    navbar close issue fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -35,12 +35,13 @@ const Navbar = () => {
             Links.map((link, index) => (
               <li key={index} className='md:ml-8 md:my-0 my-7 font-semibold'>
                 <Link to={link.link}>
-                  <button className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</button>
+                  <button onClick={() => setOpen(false)}
+                    className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</button>
                 </Link>
               </li>))
           }
           <li>
-            <button onClick={() => setShowAddHabit(true)}
+            <button onClick={() => { setShowAddHabit(true); setOpen(false) }}
               className='btn bg-blue-600 text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static'>
               Add Habit
             </button>
